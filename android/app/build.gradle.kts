c0c01c9caffc2c9d61e1d269926a6ced249e2709
davigomesflorencio <davigomesflorencio@alu.ufc.br>
2025-04-10 13:53:01 -0300

    Update version 2.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.1.0"
+        versionCode = 5
+        versionName = "2.2.0"
     }
 
     signingConfigs {
